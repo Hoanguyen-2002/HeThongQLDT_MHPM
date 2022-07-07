fb449e0c40cad92e3ab950fb3d78b2ee08b7dd0e
Trung Kiên <91403009+TrungKienz@users.noreply.github.com>
2022-07-07 22:10:55 +0700

    Update TabDemoFrame.java

--- Project/src/UTILITIES/TabDemoFrame.java
+++ Project/src/UTILITIES/TabDemoFrame.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package UTILITIES;
 
-/**
- *
- * @author Mr.Minh
- */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
