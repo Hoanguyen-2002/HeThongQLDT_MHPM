f66405203ca62bc1db2664655cfb8a0971bf6b7a
NguyenVanDuy20207666 <93897448+OLW712002@users.noreply.github.com>
2022-07-07 18:14:56 +0700

    Update DAL_Finding.java
    update DAL

--- Project/src/DAL/DAL_Finding.java
+++ Project/src/DAL/DAL_Finding.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import DBO.DBO_Finding;
 import java.util.ArrayList;
 
-public class DAL_Finding extends DataAccessHelper {
+ class DAL_Finding extends DataAccessHelper {
 
     MyMethod method = new MyMethod();
     CallableStatement store_proc = null;
