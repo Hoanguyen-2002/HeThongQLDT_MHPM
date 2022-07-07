89f66597a6f25f83a8266d07b636fdce1d674426
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2022-07-07 22:35:05 +0700

    update model lop

--- Project/src/DBO/DBO_LOP.java
+++ Project/src/DBO/DBO_LOP.java
@@ -4,6 +4,41 @@ import java.util.Date;
 
 public class DBO_LOP {
 
-    
+    private String MaLop;
+    private String TenLop;
+    private String MaKhoa;
+    private String MaGiangVien;
+
+    public String getMaLop() {
+        return MaLop;
+    }
+
+    public void setMaLop(String MaLop) {
+        this.MaLop = MaLop;
+    }
+
+    public String getTenLop() {
+        return TenLop;
+    }
+
+    public void setTenLop(String TenLop) {
+        this.TenLop = TenLop;
+    }
+
+    public String getMaKhoa() {
+        return MaKhoa;
+    }
+
+    public void setMaKhoa(String MaKhoa) {
+        this.MaKhoa = MaKhoa;
+    }
+
+    public String getMaGiangVien() {
+        return MaGiangVien;
+    }
+
+    public void setMaGiangVien(String MaGiangVien) {
+        this.MaGiangVien = MaGiangVien;
+    }
 
 }
