e75f70f1a3facdd950f362e2b8a78b56056ed1e8
NguyenVanDuy20207666 <93897448+OLW712002@users.noreply.github.com>
2022-07-07 18:16:45 +0700

    Update DAL_HeDaoTao.java
    update HeDaoTao

--- Project/src/DAL/DAL_HeDaoTao.java
+++ Project/src/DAL/DAL_HeDaoTao.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 
- class DAL_HeDaoTao extends DataAccessHelper {
+public class DAL_HeDaoTao extends DataAccessHelper {
     MyMethod method = new MyMethod();
     CallableStatement store_proc = null;
     ResultSet rs = null;
