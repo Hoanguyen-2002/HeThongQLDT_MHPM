e532d980e872c3a955a249d19c3742fc6483e49e
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2022-07-07 22:23:17 +0700

    update tin tuc

--- Project/src/DBO/DBO_TIN_TUC.java
+++ Project/src/DBO/DBO_TIN_TUC.java
@@ -2,41 +2,6 @@ package DBO;
 
 public class DBO_TIN_TUC {
 
-    private int MaTinTuc;
-    private String TieuDe;
-    private String NoiDung;
-    private String TrangThai;
-
-    public int getMaTinTuc() {
-        return MaTinTuc;
-    }
-
-    public void setMaTinTuc(int MaTinTuc) {
-        this.MaTinTuc = MaTinTuc;
-    }
-
-    public String getTieuDe() {
-        return TieuDe;
-    }
-
-    public void setTieuDe(String TieuDe) {
-        this.TieuDe = TieuDe;
-    }
-
-    public String getNoiDung() {
-        return NoiDung;
-    }
-
-    public void setNoiDung(String NoiDung) {
-        this.NoiDung = NoiDung;
-    }
-
-    public String getTrangThai() {
-        return TrangThai;
-    }
-
-    public void setTrangThai(String TrangThai) {
-        this.TrangThai = TrangThai;
-    }
+  
 
 }
